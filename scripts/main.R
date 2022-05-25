a80d4c8d1162ffeeef877c206b6c96d23ea9bc5e
Jeremie Orliac <jeremie@climatefarmers.org>
2022-05-25 16:33:33 +0200

    Rewording + graph generation

--- scripts/main.R
+++ scripts/main.R
@@ -26,16 +26,18 @@ data_loc <- file.path(parameters$data_loc)
 co2_emissions_loc <- working_dir
 
 scenario_selected = "previous_years"
-all_results_previous_year <- run_lca(inputs_loc,
+all_results_previous_years <- run_lca(inputs_loc,
         co2_emissions_loc,
         results_loc,
         data_loc = data_loc,
         scenario_selected
 )
+all_results_previous_years <- all_results_previous_years %>% rename(co2eq_previous_years=co2eq) %>%
+  select(source, gas, co2eq_previous_years)
 
-gas_summary_previous_year <- all_results_previous_year %>% 
+gas_summary_previous_years <- all_results_previous_years %>% 
   group_by(gas) %>% 
-  summarise(co2eq = sum(co2eq), .groups = "drop")
+  summarise(co2eq = sum(co2eq_previous_years), .groups = "drop")
 
 scenario_selected = "current"
 all_results_current <- run_lca(inputs_loc,
@@ -44,17 +46,36 @@ all_results_current <- run_lca(inputs_loc,
                        data_loc = data_loc,
                        scenario_selected
                        )
-gas_summary_current <- all_results_current %>% 
-  group_by(gas) %>% 
-  summarise(co2eq = sum(co2eq), .groups = "drop")
+all_results_current <- all_results_current %>% rename(co2eq_current=co2eq) %>%
+  select(source, gas, co2eq_current)
 
-plot_source_breakdown(all_results , results_loc)#%>%  filter(source != "leakage")
+gas_summary_current <- all_results_current %>%  group_by(gas) %>% 
+  summarise(co2eq = sum(co2eq_current), .groups = "drop")
 
-write_csv(gas_summary, file.path(results_loc, "lca_results.csv"))
-write_csv(all_results, file.path(results_loc, "lca_results_source.csv"))
+plot_source_breakdown(all_results_current  %>% rename(co2eq=co2eq_current) %>% filter (source!="leakage"), results_loc)#%>%  filter(source != "leakage")
 
+write_csv(gas_summary_current, file.path(results_loc, "CO2eq_emissions_2021.csv"))
+write_csv(all_results_current, file.path(results_loc, "CO2eq_emissions_source_2021.csv"))
 
+write_csv(gas_summary_previous_years, file.path(results_loc, "CO2eq_emissions_2020.csv"))
+write_csv(all_results_previous_years, file.path(results_loc, "CO2eq_emissions_source_2020.csv"))
 
+all_results <- inner_join(all_results_previous_years, all_results_current, by="source") %>%
+  mutate(co2eq_reductions=co2eq_previous_years-co2eq_current)%>%
+  mutate(reduction_or_increase=ifelse(co2eq_previous_years-co2eq_current>0,"reduction","increase"))
+
+write_csv(all_results, file.path(results_loc, "diff_btw_2021_2020.csv"))
+
+name <-"Reductions_bar_chart"
+graph <- ggplot(data = all_results %>% filter (source!="leakage"), aes(x=source, fill = reduction_or_increase))+
+  geom_bar(aes(y = co2eq_reductions*1e-3), stat="identity",  alpha=0.7)+   
+  labs(title = name)+
+  xlab("Time")+
+  ylab("tCO2 avoided")
+print(graph)
+png(file.path(results_loc,paste(name,".png",sep="")))
+print(graph)
+dev.off()
 
 
 
