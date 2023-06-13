105966d3c41c6a13d6767a8bceeb54b1a481b755
JeremieO <jeremie@climatefarmers.org>
2023-06-13 12:28:03 +0200

    ignoring the difference in indirect n2o emissions between grazing and stable management

--- scripts/calc_functions.R
+++ scripts/calc_functions.R
@@ -129,7 +129,7 @@ n2o_manure_deposition_indirect <- function( # add non-grazing days
   
   if(nrow(animal_data) > 0){
     animal_data <- animal_data %>% 
-      mutate(n2o_urine_dung_indirect = n_animals * grazing_days/365 * n_excretion_rate_kg_1000am * 365 * mass_kg_per_animal/1000 * 
+      mutate(n2o_urine_dung_indirect = n_animals * n_excretion_rate_kg_1000am * 365 * mass_kg_per_animal/1000 
                (frac_gasm * ef_4 + frac_leach * ef_5))
   }else{
     warning("No Animal data provided - or included in project")
